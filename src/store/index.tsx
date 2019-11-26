785f36a22da0691143e1c267d52ea44529297386
Vadim <vadim.putrov@gmail.com>
2019-11-26 13:40:24 +0200

    add redux devtols

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,10 @@
 import {createStore} from 'redux'
 import rootReducer from '../reducers/index'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
-    rootReducer
+    rootReducer,
+    composeWithDevTools()
 )
 
 export default store
