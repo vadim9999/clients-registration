2db03e9817b978833b275d51cd3efe3cbc00e53e
Vadim <vadim.putrov@gmail.com>
2019-11-26 14:32:52 +0200

    add json server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from '../store/index'
 // import '../styles/index.css';
 import ClientList from './ClientList/ClientList'
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+import 'antd/dist/antd.css';
 
 class App extends React.PureComponent {
-	render() {
-		return (
-			<div>
+  render() {
+    return (
+      <div>
         <Provider store={store}>
-          <ClientList />
+          <Layout style={{height:'100vh'}}>
+            <Header>Header</Header>
+            <Content><ClientList /></Content>
+            <Footer>Footer</Footer>
+          </Layout>
+          
         </Provider>
-			  
-			</div>
-		);
-	}
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ClientList/ClientList.tsx
+++ src/components/ClientList/ClientList.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-
+import { Table, Divider, Tag } from 'antd';
+import data from './clients'
 interface ClientListProps {
 
 }
+
+const columns = [
+  {
+    title:'Name',
+    dataIndex:'name',
+    key:'name'
+  },
+  {
+    title: 'Registration date',
+    dataIndex: 'date',
+    key:'date'
+  }
+]
+
+
 class ClientList extends React.Component<ClientListProps, {} >{
 
   render() {
     return (
-      <a>Hi</a>
+      <Table columns = {columns} dataSource={data}/>
     )
   }
 }
